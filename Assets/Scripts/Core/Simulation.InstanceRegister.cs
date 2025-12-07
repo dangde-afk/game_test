7ebedfe3d0164c36f99c530cbb36be7698679997
danggianghai04092005 <141430216+dangde-afk@users.noreply.github.com>
2025-12-07 20:36:13 +0700

    update game ui menu starting

--- Assets/Scripts/Core/Simulation.InstanceRegister.cs
+++ Assets/Scripts/Core/Simulation.InstanceRegister.cs
@@ -1,16 +1,16 @@
-﻿namespace Platformer.Core
-{
-    public static partial class Simulation
+﻿    namespace Platformer.Core
     {
-        /// <summary>
-        /// This class provides a container for creating singletons for any other class,
-        /// within the scope of the Simulation. It is typically used to hold the simulation
-        /// models and configuration classes.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        static class InstanceRegister<T> where T : class, new()
+        public static partial class Simulation
         {
-            public static T instance = new T();
+            /// <summary>
+            /// This class provides a container for creating singletons for any other class,
+            /// within the scope of the Simulation. It is typically used to hold the simulation
+            /// models and configuration classes.
+            /// </summary>
+            /// <typeparam name="T"></typeparam>
+            static class InstanceRegister<T> where T : class, new()
+            {
+                public static T instance = new T();
+            }
         }
     }
-}
